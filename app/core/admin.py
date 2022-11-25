f86417ed57414e4fcf644e74253bad4c245ea925
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2022-11-25 17:18:30 -0300

    Setup Django Admin User

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,41 @@
+"""
+Django admin customization
+"""
+from core import models
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+
+class UserAdmin(BaseUserAdmin):
+    """Define para usuários a página de admin"""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permições', {'fields': (
+            'is_active',
+            'is_staff',
+            'is_superuser'
+        )}),
+        ('Datas importantes', {'fields': (
+            'last_login',
+        )})
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
+    
+
+admin.site.register(models.User, UserAdmin)
